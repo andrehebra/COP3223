cc4e94ea7c7cb7c20d14c1b557636e1ae74d0d5d
andrehebra <andrehebra@gmail.com>
2023-01-09 15:11:04 -0500

    created hello world

---
+++ helloWorld/hello.c
@@ -0,0 +1,13 @@
+/* Arup Guha
+  My First C Program
+  8/23/05
+  COP 3223 Rocks! */
+
+#include <stdio.h>
+
+int main(void) {
+
+  printf("COP 3223 Rocks!\n");
+  printf("This class is too basic for me this is so easy I thought that it would be easier");
+  return 0;
+}
