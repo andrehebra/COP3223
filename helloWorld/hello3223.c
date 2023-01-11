3f4cdb1f1d0ff628f7a002a8173611dd07cb7768
andrehebra <52555960+andrehebra@users.noreply.github.com>
2023-01-11 17:59:58 +0000

    assignment 0 completed

---
+++ helloWorld/hello3223.c
@@ -0,0 +1,14 @@
+/*
+  Andre Hebra
+  COP3223C Section 3
+  Assignment #0 Hello World!
+  Program prints "Hello 3223 class!" to the terminal
+*/
+
+#include <stdio.h>
+
+int main(void) {
+
+  printf("Hello 3223 class!\n");
+  return 0;
+}
