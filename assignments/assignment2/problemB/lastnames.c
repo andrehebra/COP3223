a274dd4c167057b11a6e04c8b349d981160a63a1
andrehebra <52555960+andrehebra@users.noreply.github.com>
2023-02-03 00:09:07 +0000

    calendar activity

--- assignments/assignment2/problemB/lastnames.c
+++ assignments/assignment2/problemB/lastnames.c
@@ -5,8 +5,9 @@ int main() {
     char names;
 
     /*ask user for n and the last names*/
-    printf("Enter n, followed by n Last names (each last name must be a single word");
-    scanf("%d", &n);
+    printf("Enter n, followed by n Last names (each last name must be a single word):\n");
+    scanf("%c", &names);
+    printf("%c", names);
 
 
     return 0;
--- labs/lab3/calendar.c
+++ labs/lab3/calendar.c
@@ -3,20 +3,35 @@
 int main() {
 
     int calendar_entry;
-    int i, start=3, days=31;
+    int i, start=7, days=31;
+    int week_counter = 1;
+    int day_counter = 1;
 
-    for (i=0; i<start; i++) {
+    for (i=1; i<start; i++) {
         printf("...");
     }
 
     for (i=0; i<days; i++) {
         calendar_entry= (i+start);
 
-        printf(" %d", i + 1);
+
+        if (week_counter==3 && day_counter==4) {
+            printf("   ");
+        } else {
+            printf(" %2d", i + 1);
+        }
+        
+        
+
+        day_counter++;
         if (calendar_entry%7==0) {
             printf("\n");
+            week_counter++;
+            day_counter=1;
         }
     }
+
+    printf("\n");
 
     return 0;
 }
