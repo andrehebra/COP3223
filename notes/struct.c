a38d2312492a01de35839f8232b729dbfaba4ee7
andrehebra <andrehebra@gmail.com>
2023-03-06 15:15:55 -0500

    struct notes

---
+++ notes/struct.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+struct gas {
+    float distance;
+    float gals;
+    float mpg;
+    float gas;
+};
+
+int main() {
+    struct gas first;
+
+    scanf("%f", &first.distance);
+
+    return 0;
+}
